951ea61d02c09410b393ec85da6d0be050e0a826
pavithra0206 <47376964+pavithra0206@users.noreply.github.com>
2019-02-09 23:39:08 -0800

    Update program3.java

--- program3.java
+++ program3.java
@@ -11,10 +11,10 @@ System.out.println("Enter a character : ");
 char ch=sc.next( ).charAt(0);	 		
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 {
-System.out.println("Entered character "+ch+" is  Vowel"); 
+System.out.println("Vowel"); 
 }
 else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-System.out.println("Entered character "+ch+" is Consonant");
+System.out.println("Consonant");
 else
 System.out.println("Invalid");		
 }
